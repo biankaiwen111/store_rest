1bbab7e76a40d11e6068eb43da43ecc9f81d84ce
biankaiwen111 <biankaiwen111@outlook.com>
2022-01-02 02:34:54 -0800

    fix config

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
@@ -13,11 +12,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "kevishax"
 api = Api(app)
-
-@app.before_first_request
-def create_table():
-    db.create_all()
-
 
 
 jwt = JWT(app, authenticate, identity)
